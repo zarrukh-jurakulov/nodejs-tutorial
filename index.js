b2690fc7b6bb9d440019776ce6dde2e44b7ed320
zarrukh_jurakulov <zarrukhjurakulov474@gmail.com>
2023-06-05 14:48:33 +0500

    web-server

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const http = require("http");
 
-const server = http.createServer((request, response) => {
-  // request -  so'rov
-  // response - javobi
-  console.log(request.url);
+const server = http.createServer((req, res) => {
+  //   // req -  so'rov
+  //   // req - javobi
+  //   console.log(request.url);
 
-  response.write("<h1>Hello World 14</h1>");
-  response.end();
+  //   //   res.write("<h1>Hello World</h1>");
+  //   res.end("<h1>Hello World</h1>");
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`
+    <h2>Send name</h2>
+    <form method="post" action="/">
+        <input type="text" name="name" placeholder="Enter your name"/>
+        <button type="submit">Send name</button>
+    </form>
+    `);
+  } else if (req.method === "POST") {
+    const body = [];
+    res.writeHead(200, { "Content-Type:": "text/html; charset=utf-8" });
+
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+      res.end(`Name successfully added: ${message}`);
+    });
+  }
 });
 
 server.listen(3001, () => {
