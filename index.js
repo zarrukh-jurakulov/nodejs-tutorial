2c4d021f038ca70c312793e10efe77f4f80f0fed
zarrukh_jurakulov <zarrukhjurakulov474@gmail.com>
2023-06-05 15:39:04 +0500

    working with html

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
   //   // req -  so'rov
@@ -7,15 +9,38 @@ const server = http.createServer((req, res) => {
 
   //   //   res.write("<h1>Hello World</h1>");
   //   res.end("<h1>Hello World</h1>");
+
   if (req.method === "GET") {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end(`
-    <h2>Send name</h2>
-    <form method="post" action="/">
-        <input type="text" name="name" placeholder="Enter your name"/>
-        <button type="submit">Send name</button>
-    </form>
-    `);
+
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "templates", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    } else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "templates", "about.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    } else if (req.url === "/contact") {
+      fs.readFile(
+        path.join(__dirname, "templates", "contact.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    }
   } else if (req.method === "POST") {
     const body = [];
     res.writeHead(200, { "Content-Type:": "text/html; charset=utf-8" });
